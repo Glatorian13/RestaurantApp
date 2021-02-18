1482642c0d7b1f9a91fcb220c746947d7349847f
colemilwee <rocolebot@gmail.com>
2021-02-17 20:09:57 -0500

    adding javascript

---
+++ data/waitinglist.js
@@ -0,0 +1,10 @@
+const waitingArray = [
+    {
+        customerName: 'kaitlyn',
+        customerEmail: 'katlyn@example',
+        phoneNumber: '000-000-0000',
+        customerID: 'Copper',
+    },
+];
+
+module.exports = waitingArray
